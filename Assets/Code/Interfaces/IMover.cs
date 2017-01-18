2d9ff91dbe8bba0f9e0bae051c25bc180b8b9350
TAMK\Kalle Heinonen <kalle.heinonen@cs.tamk.fi>
2017-01-18 11:01:05 +0200

    Added UnitBase script & IMover interface

---
+++ Assets/Code/Interfaces/IMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SpaceShooter
+{
+    public interface IMover
+    {
+        //The position of this object in world space
+        Vector3 Position { get; set; }
+
+        //The rotation of this object in world space
+        Quaternion Rotation { get; set; }
+
+        //The speed of this mover
+        float Speed { get; }
+
+        //Move toward targetPosition
+        void MoveTowardPosition(Vector3 targetPosition);
+
+        //Move to direction
+        void MoveToDirection(Vector3 direction);
+
+        //Rotate toward targetPosition
+        void RotateTowardPosition(Vector3 targetPosition);
+    }
+}
