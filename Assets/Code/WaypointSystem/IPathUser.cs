d9a779e6de29ad912aaf49f8a1032d311fab3d2f
TAMK\Kalle Heinonen <kalle.heinonen@cs.tamk.fi>
2017-03-08 11:01:09 +0200

    WaypointSystem is here. Or the beginnings, at least.

---
+++ Assets/Code/WaypointSystem/IPathUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpaceShooter.WaypointSystem
+{
+    public enum Direction
+    {
+        Forward,
+        Backward
+    }
+
+    public enum PathType
+    {
+        Loop,
+        PingPong,
+        OneWay
+    }
+
+    interface IPathUser
+    {
+        Waypoint CurrentWaypoint { get; }
+        Direction Direction { get; set; }
+        Vector3 Position { get; set; }
+
+        void Init(IMover mover, Path path);
+    }
+}
---
+++ Assets/Code/WaypointSystem/Path.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter.WaypointSystem
+{
+    public class Path : MonoBehaviour
+    {
+        [SerializeField]
+        private PathType _pathType;
+
+        private readonly Dictionary<PathType, Color> _pathColors =
+            new Dictionary<PathType, Color>
+            {
+                {PathType.Loop, Color.green },
+                {PathType.PingPong, Color.red },
+                {PathType.OneWay, Color.blue }
+            };
+
+        private List<Waypoint> _waypoints;
+
+        public List<Waypoint> Waypoints
+        {
+            get
+            {
+                if (_waypoints == null || _waypoints.Count == 0 || !Application.isPlaying)
+                {
+                    _waypoints = GetComponentsInChildren<Waypoint>().ToList();
+                }
+
+                return _waypoints;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _pathColors[_pathType];
+
+            if(Waypoints.Count > 1)
+            {
+                for(int i = 1; i < Waypoints.Count; ++i)
+                {
+                    Gizmos.DrawLine(Waypoints[i - 1].Position, Waypoints[i].Position);
+                }
+
+                if (_pathType == PathType.Loop)
+                {
+                    Gizmos.DrawLine(Waypoints[Waypoints.Count - 1].Position, Waypoints[0].Position);
+                }
+            }
+        }
+    }
+}
